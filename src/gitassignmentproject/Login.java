0e159777658b15223025f63777a7de468f4614b8
Uarifuna <uarifuna14@gmail.com>
2026-04-23 12:28:12 +0200

    Added Login class to feature-login branch

---
+++ src/gitassignmentproject/Login.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitassignmentproject;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Login successful. Welcome to the system!");
+    }
+}
