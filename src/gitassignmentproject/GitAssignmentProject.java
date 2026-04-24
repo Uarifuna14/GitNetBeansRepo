9c64185fcd5f72a9cfb2092f922c397e6dab50df
Uarifuna <uarifuna14@gmail.com>
2026-04-24 10:51:24 +0200

    Updated gitass.java

--- src/gitassignmentproject/GitAssignmentProject.java
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -16,7 +16,7 @@ public class GitAssignmentProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+        System.out.println("Main branch version");
     }
     
 }
